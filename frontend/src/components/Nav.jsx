7cd0db31f53abe8f57ff9295937252d82a138f0c
Somya Rani <somyarani698@gmail.com>
2026-02-02 17:21:53 +0530

    Fix navbar city display and add city hook

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,35 +1,135 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 import { LuShoppingCart } from "react-icons/lu";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { RxCross1 } from "react-icons/rx";
+import axios from "axios";
+import { serverUrl } from "../App";
+import { setUserData } from "../redux/userSlice";
 
 function Nav() {
-  return (
-   <div className="w-full h-20 flex items-center justify-between
-   md:justify-center gap-7.5 px-5 fixed top-0 z-9999 bg-[#fff9f6]
-   overflow-visible ">
-    <h1 className="text-3xl font-bold mb-2 text-[#ff4d2d] ">Vingo</h1>
-    <div className="md:w-[60%] lg:w-[40%] h-17.5 bg-white shadow-xl
-    rounded-lg items-center gap-5 flex ">
-        <div className="flex items-center w-[30%] overflow-hidden
-        gap-2.5 px-2.5 border-r-2 border-gray-400 ">
-             <FaLocationDot size={25} className="text-[#ff4d2d]" />
-             <div className="w-20 truncate text-gray-500 ">Jhansi</div>
+  const { userData, city } = useSelector((state) => state.user);
+  const [showInfo, setShowInfo] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+  const dispatch = useDispatch();
+  const handleLogOut = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/auth/signout`, {
+        withCredentials: true,
+      });
 
+      dispatch(setUserData(null));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div
+      className="w-full h-20 flex items-center justify-between
+   md:justify-center gap-7.5 px-5 fixed top-0 z-9999 bg-[#fff9f6]
+   overflow-visible "
+    >
+      {showSearch && (
+        <div
+          className="w-[90%] h-17.5 fixed bg-white shadow-xl
+    rounded-lg items-center gap-5 flex top-20 left-[5%] md:hidden"
+        >
+          <div
+            className="flex items-center w-[30%] overflow-hidden
+        gap-2.5 px-2.5 border-r-2 border-gray-400 "
+          >
+            <FaLocationDot size={25} className="text-[#ff4d2d]" />
+            <div className="w-20 truncate text-gray-500 ">
+              {city}
+            </div>
+          </div>
+          <div className="w-[80%] flex items-center gap-2.5">
+            <FaSearch size={25} className="text-[#ff4d2d]" />
+            <input
+              type="text"
+              placeholder="search delicious food..."
+              className="px-2.5 text-gray-700
+            outline-0 w-full "
+            />
+          </div>
+        </div>
+      )}
+
+      <h1 className="text-3xl font-bold mb-2 text-[#ff4d2d] ">Vingo</h1>
+      <div
+        className="md:w-[60%] lg:w-[40%] h-17.5 bg-white shadow-xl
+    rounded-lg items-center gap-5 hidden md:flex "
+      >
+        <div
+          className="flex items-center w-[30%] overflow-hidden
+        gap-2.5 px-2.5 border-r-2 border-gray-400 "
+        >
+          <FaLocationDot size={25} className="text-[#ff4d2d]" />
+          <div className="w-20 truncate text-gray-500 ">{city}</div>
         </div>
         <div className="w-[80%] flex items-center gap-2.5">
-            <FaSearch size={25} className="text-[#ff4d2d]" />
-            <input type="text" placeholder="search delicious food..." className="px-2.5 text-gray-700
-            outline-0 w-full " />
-
+          <FaSearch size={25} className="text-[#ff4d2d]" />
+          <input
+            type="text"
+            placeholder="search delicious food..."
+            className="px-2.5 text-gray-700
+            outline-0 w-full "
+          />
         </div>
+      </div>
+      <div className="flex items-center gap-4">
+        {showSearch ? (
+          <RxCross1
+            className="text-[#ff4d2d] md:hidden"
+            onClick={() => setShowSearch(false)}
+          />
+        ) : (
+          <FaSearch
+            size={25}
+            className="text-[#ff4d2d] md:hidden"
+            onClick={() => setShowSearch(true)}
+          />
+        )}
 
+        <div className="relative cursor-pointer">
+          <LuShoppingCart size={25} className="text-[#ff4d2d]" />
+          <span className="absolute -right-2.25 -top-3 text-[#ff4d2d] ">0</span>
+        </div>
+        <button
+          className="hidden md:block px-3 py-1 rounded-lg bg-[#ff4d2d]/10 text-[#ff4d2d]
+    text-sm font-medium"
+        >
+          My Orders
+        </button>
+        <div
+          className="w-10 h-10 rounded-full flex items-center
+    justify-center bg-[#ff4d2d] text-white text-[18px] shadow-xl font-semibold cursor-pointer"
+          onClick={() => setShowInfo((prev) => !prev)}
+        >
+          {userData?.fullName?.slice(0, 1) || "Guest"}
+        </div>
+        {showInfo && (
+          <div
+            className="fixed top-20 right-2.5 md:right-[10%] lg:right-[25%]
+    w-45 bg-white shadow-2xl rounded-xl p-5 flex flex-col gap-2.5 z-9999 "
+          >
+            <div className="text-[17px] font-semibold ">
+              {userData.fullName}
+            </div>
+            <div className="text-[#ff4d2d] font-semibold cursor-pointer md:hidden">
+              My Orders
+            </div>
+            <div
+              className="text-[#ff4d2d] font-semibold cursor-pointer"
+              onClick={handleLogOut}
+            >
+              Log Out
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-    <div className="relative cursor-pointer">
-    <LuShoppingCart size={25} className="text-[#ff4d2d]"/>
-    <span className="absolute -right-2.25 -top-3 text-[#ff4d2d] ">0</span>
-    </div>
-
-   </div>
   );
 }
 
